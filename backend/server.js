2badbc1fb98983e9960a1887812c1a5be4ff662f
Jashan Gill <48613250+Jashann@users.noreply.github.com>
2025-10-28 22:20:39 -0500

    git changes

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,7 @@ app.use(cookieParser());
 
 // MongoDB Connection
 mongoose
-  .connect(
-    'mongodb+srv://jashangill3592:4GQyPIRwT6lm5IiE@cluster0.p23io.mongodb.net/Money-Lens-MongoDB?retryWrites=true&w=majority'
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB Connected'))
   .catch((err) => console.error('❌ MongoDB Connection Error:', err));
   
